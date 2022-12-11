ee256f4123fbb0cf57532287dfb56320f7969e4f
DewGoody <dewzky1@gmail.com>
2022-12-12 01:50:51 +0700

    setup search page

--- public/src/search.js
+++ public/src/search.js
@@ -1,15 +1,16 @@
 import {get_hotel_list} from "/public/src/firestoreAPI.js"
 
+function clickSearch() {
+    var where = document.getElementById("w").value;
+    var people = document.getElementById("people").value;
+    window.location.href='/public/web/search.html?'+where+"&"+people;
+}
+window.clickSearch = clickSearch;
 function getDBHotel () {
-    // var input = window.location.href.slice(window.location.href.indexOf("?")+1,window.location.href.length).split("&")
-    // console.log(input);
-    // var str = decodeURIComponent(escape(input[0]));
-    // console.log(str);
-    // get_hotel_list(input[0],"",input[1])
-
-    // let name = input[0];
-    // let guest = input[1];
-    get_hotel_list("nut","","2").then((result)=>{
+    var input = window.location.href.slice(window.location.href.indexOf("?")+1,window.location.href.length).split("&")
+    var address = decodeURIComponent(input[0].replace(/\+/g," "));
+    console.log(address);
+    get_hotel_list(address,"",input[1]).then((result)=>{
         console.log(result);
         for(let i=0; i<result.length;i++ ){
             document.getElementById("name").innerHTML = result["address"];

e3b07f158c2ea6c07124161e862c48d83e1ddd71
nutnai <nutnai3302@gmail.com>
2022-12-12 22:17:51 +0700

    almost finish api

--- public/src/firestoreAPI.js
+++ public/src/firestoreAPI.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
-import { getFirestore, query, collection, getDocs, doc, setDoc, addDoc, where} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { getFirestore, query, collection, getDocs, doc, setDoc, addDoc, where, getDoc, updateDoc} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
 
 // web app's Firebase configuration
 const firebaseConfig = {
@@ -25,7 +25,9 @@ export async function get_hotel_list(address, time, number_of_customer) {
     const querySnapshot = await getDocs(q);
     var ret = []
     querySnapshot.forEach((doc) => {
-        ret.push(doc.data());
+        var data = doc.data();
+        data["id"] = doc.id;
+        ret.push(data);
         // console.log(doc.id, " => ", doc.data());
     });
     return ret;
@@ -35,15 +37,10 @@ export async function get_hotel_list(address, time, number_of_customer) {
 //ดูข้อมูลคอมเมนต์ตอนแสดงรายละเอียดโรงแรม
 export async function get_review (id_review) {
     const q = query(collection(db, "review_list"));
-    const querySnapshot = await getDocs(q);
-    var ret = new Object()
-    querySnapshot.forEach((doc) => {
-        ret["id"] = doc.id
-        ret["data"] = doc.data()
-    });
-    return ret;
+    const querySnapshot = await getDoc(doc(db, "review_list", id_review));
+    return querySnapshot.data()
 }
-// get_review("Z15fDv3tWZevZhSrUG0p").then((result)=>{console.log(result.data);})
+// get_review("Z15fDv3tWZevZhSrUG0p").then((result)=>{console.log(result);})
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////add
 function createMapPrice (number_of_customer, price){
@@ -53,19 +50,28 @@ function createMapPrice (number_of_customer, price){
     }
     return ret;
 }
-function calculateStar(review) {
-    var ret = 0;
-    review.forEach(element => {
-        var temp = 0;
-        them += await get_review(element)
-        ret += temp
-    });
-    console.log(ret);
-    ret/=review.length
-    console.log(ret);
-    return ret.toFixed(2);
+async function getStarFromReview (id_review) {
+    const reviewSnapshot = await getDoc(doc(db, "review_list", id_review));
+    return reviewSnapshot.data().star;
 }
-console.log(calculateStar(["Z15fDv3tWZevZhSrUG0p"]));
+async function calculateStar (id_hotel, comment_list) {
+    const hotelSnapshot = await getDoc(doc(db, "hotel_list", id_hotel));
+    var sumStar = 0
+    comment_list.forEach(review => {
+        sumStar += review.star;
+    });
+    sumStar /= comment_list.length;
+    sumStar = sumStar.toFixed(2);
+    await updateDoc(doc(db, "hotel_list", id_hotel), {star : sumStar});
+}
+async function addReview(id_review, id_hotel) {
+    const hotelSnapshot = await getDoc(doc(db, "hotel_list", id_hotel));
+    const reviewlSnapshot = await getDoc(doc(db, "review_list", id_review));
+    var comment_list = hotelSnapshot.data().review
+    comment_list.push({id:id_review, star:reviewlSnapshot.data().star})
+    await updateDoc(doc(db, "hotel_list", id_hotel), {review : comment_list});
+    calculateStar(id_hotel, comment_list)
+}
 ////////////////////////////////////////config
 //hotel
 async function add_hotel_list(name, address, number_of_customer, price, facility, review, image, piority, star, id_landlord) {
@@ -89,32 +95,33 @@ async function thotel () {
     var price = [600,700,1200];
     var facility = ["เตียง", "ทีวี", "ตู้เย็น", "แอร์"];
     var review = [await add_review("zqF2ASPu6tISEcg7dkyC",5.0,"ดีคับ")];
-    console.log(await add_review("zqF2ASPu6tISEcg7dkyC",5.0,"ดีคับ"));
     var image = ["ht_1", "ht_2", "ht_3"];
     var piority = 0;
-    var star = calculateStar(review);
+    var star = 5;
     var id_landlord = "Aiu7zv5DXjBFfWHfE1co";
     add_hotel_list(name, address, number_of_customer, createMapPrice(number_of_customer,price), facility, review, image, piority, star, id_landlord);
+    //
 }
 window.thotel = thotel;
 
 //review
-async function add_review (id_user_review, star, comment) {
+async function add_review (id_user_review, star, comment, id_hotel) {
     var ret;
     await addDoc(collection(db, "review_list"), {
         id_user_review : id_user_review,
         star : star,
         comment : comment
     }).then((doc) => {
+        addReview(doc.id, id_hotel);
         ret = doc.id
     });
     return ret;
 }
-function treview () {
+async function treview () {
     var id_user_review = "zqF2ASPu6tISEcg7dkyC";
-    var star = 4.32;
-    var comment = "ปีศาจกุ้ง";
-    add_review(id_user_review, star, comment);
+    var star = 4.69;
+    var comment = "มีแบบ15นาทีมั้ยครับ";
+    add_review(id_user_review, star, comment, "cen38MZ8YL0GSxk37Kq5")
 }
 window.treview = treview;
 //user
@@ -135,14 +142,10 @@ window.tuser = tuser;
 
 //
 
-function testAdd () {
-    addDoc(collection(db,"test"), {
-        test : "ha"
-    }).then((result)=>{
-        console.log(result.id);
-    })
+function tt () {
+    addReview("3jfEFge1VGFnWFFBbfHs", "kL4RXrXy3gSdfUxzNv2a")
 }
-window.testAdd = testAdd
+window.tt = tt;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////set
 
